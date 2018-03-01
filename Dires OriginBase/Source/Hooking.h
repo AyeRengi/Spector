39184bb3fcfb6ff417d68b6b2daeb3ebe3cc82dd
Alejandro Rengifo <ARengifo07@gmail.com>
2018-02-28 21:33:01 -0500

    Origin Base (with natives updated to 1.42)
    already updated the natives, but still needs a lot of work just to be able to inject it. (ex. Needs hooking method, update crossmap, etc).

---
+++ Dires OriginBase/Source/Hooking.h
@@ -0,0 +1,67 @@
+#pragma once
+
+class Hooking
+{
+public:
+	static void Start(HMODULE hmoduleDLL);
+	static void Cleanup();
+	static eGameState GetGameState();
+	static BlipList* GetBlipList();
+	static uint64_t getWorldPtr();
+	static void onTickInit();
+	static bool HookNatives();
+
+	// Native function handler type
+	typedef void(__cdecl * NativeHandler)(scrNativeCallContext * context);
+	struct NativeRegistrationNew {
+		uint64_t nextRegistration1;
+		uint64_t nextRegistration2;
+		ScriptEngine::NativeHandler handlers[7];
+		uint32_t numEntries1;
+		uint32_t numEntries2;
+		uint64_t hashes;
+
+		inline NativeRegistrationNew* getNextRegistration() {
+			uintptr_t result;
+			auto v5 = reinterpret_cast<uintptr_t>(&nextRegistration1);
+			auto v12 = 2i64;
+			auto v13 = v5 ^ nextRegistration2;
+			auto v14 = (char *)&result - v5;
+			do
+			{
+				*(DWORD*)&v14[v5] = v13 ^ *(DWORD*)v5;
+				v5 += 4i64;
+				--v12;
+			} while (v12);
+
+			return reinterpret_cast<NativeRegistrationNew*>(result);
+		}
+
+		inline uint32_t getNumEntries() {
+			return ((uintptr_t)&numEntries1) ^ numEntries1 ^ numEntries2;
+		}
+
+		inline uint64_t getHash(uint32_t index) {
+
+			auto naddr = 16 * index + reinterpret_cast<uintptr_t>(&nextRegistration1) + 0x54;
+			auto v8 = 2i64;
+			uint64_t nResult;
+			auto v11 = (char *)&nResult - naddr;
+			auto v10 = naddr ^ *(DWORD*)(naddr + 8);
+			do
+			{
+				*(DWORD *)&v11[naddr] = v10 ^ *(DWORD*)(naddr);
+				naddr += 4i64;
+				--v8;
+			} while (v8);
+
+			return nResult;
+		}
+	};
+	static NativeHandler GetNativeHandler(uint64_t origHash);
+
+private:
+	static BOOL InitializeHooks();
+	static void FindPatterns();
+	static void FailPatterns(const char* name, Memory::pattern ptn);
+};	void WAIT(DWORD ms);
